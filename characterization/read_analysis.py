682a8f64ad009760099c0c60e822c99d856383b4
SaberHQ <saber.hafezqorani@gmail.com>
2018-03-12 17:37:48 -0700

    - read the annotation gtf/gff3 file. - align reads to reference genome and transcriptome using minimap2. - read the alignment files and perform read analysis.

--- characterization/read_analysis.py
+++ characterization/read_analysis.py
@@ -21,6 +21,8 @@ except ImportError:
 import sys
 import os
 import getopt
+import HTSeq
+import pysam
 import numpy
 import head_align_tail_dist as align
 import get_besthit
@@ -34,7 +36,10 @@ def usage():
                     "-h : print usage message\n" \
                     "-i : training ONT real reads, must be fasta files\n" \
                     "-r : reference genome of the training reads\n" \
-                    "-m : User can provide their own alignment file, with maf extension\n" \
+                    "-t : reference transcriptome of the training reads\n" \
+                    "-a : reference GTF/GFF3 annotation files\n" \
+                    "-sg : User can provide their own genome alignment file, with sam extension\n" \
+                    "-st : User can provide their own transcriptome alignment file, with sam extension\n" \
                     "-b : number of bins (for development), default = 20\n" \
                     "-o : The prefix of output file, default = 'training'\n"
 
@@ -62,10 +67,16 @@ def main(argv):
             sys.exit(0)
         elif opt in ("-i", "--infile"):
             infile = arg
-        elif opt in ("-r", "--ref"):
-            ref = arg
-        elif opt == "-m":
-            maf_file = arg
+        elif opt in ("-r", "--ref_g"):
+            ref_g = arg
+        elif opt in ("-t", "--ref_t"):
+            ref_t = arg
+        elif opt in ("-a", "--annotation"):
+            annot = arg
+        elif opt == "-sg":
+            alignment_genome = arg
+        elif opt == "-st":
+            alignment_transcriptome = arg
         elif opt in ("-o", "--outfile"):
             outfile = arg
         elif opt == "--no_model_fit":
@@ -76,14 +87,13 @@ def main(argv):
             usage()
             sys.exit(1)
 
-    if infile == '' or ref == '':
-        print("Please specify the training reads and its reference genome!")
+    if infile == '' or ref_g == '' or ref_t == '' or annot == '':
+        print("Please specify the training reads and its reference genome and transcriptome along with the annotation GTF/GFF3 files!")
         usage()
         sys.exit(1)
 
     # READ PRE-PROCESS AND UNALIGNED READS ANALYSIS
     sys.stdout.write(strftime("%Y-%m-%d %H:%M:%S") + ": Read pre-process and unaligned reads analysis\n")
-    out_maf = outfile + ".maf"
 
     # Read pre-process
     in_fasta = outfile + ".fasta"
@@ -104,34 +114,85 @@ def main(argv):
 
     del dic_reads
 
-    # if maf file provided
-    if maf_file != '':
-        if out_maf == maf_file:
-            out_maf = outfile + "_processed.maf"
+    # Read the annotation GTF/GFF3 file
+    sys.stdout.write(strftime("%Y-%m-%d %H:%M:%S") + ": Parse the annotation file (GTF/GFF3)\n")
+    # If gtf provided, convert to GFF3 (gt gtf_to_gff3)
+    annot_filename = annot.split(".")[0]
+    annot_type = annot.split(".")[1]
+    if annot_type.upper() == "GTF":
+        call("gt gtf_to_gff3 -tidy -o " + annot_filename + ".gff3" + annot, shell=True)
 
-        call("grep '^s ' " + maf_file + " > " + out_maf, shell=True)
 
-        # get best hit and unaligned reads
-        unaligned_length = list(get_besthit.besthit_and_unaligned(in_fasta, out_maf, outfile))
+    # Next, add intron info into gff3:
+    call("gt gff3 -tidy -retainids -checkids -addintrons -o " + annot_filename + "_addedintron.gff3 " + annot_filename + ".gff3", shell=True)
+    features = HTSeq.GenomicArrayOfSets("auto", stranded=False)
+    dict_intron_info = {}
+    annot_gff3_withintron = annot_filename + "_addedintron.gff3"
+    for feature in annot_gff3_withintron:
+        if "Parent" in feature.attr:
+            info = feature.attr["Parent"].split(':')
+            if info[0] == "transcript":
+                feature_id = info[1]
+                if feature_id not in dict_intron_info:
+                    dict_intron_info[feature_id] = []
+        if feature.type == "intron":
+            # feature_id_2 = feature.name.split(':')[1] #feature_id_2 is same as feature_id above if feature is intron, I was just checking and testing it. then removed this line.
+            features[feature.iv] += feature_id
+            dict_intron_info[feature_id].append((feature.iv.start, feature.iv.end, feature.iv.length))
 
-    # if maf file not provided
-    else:
-        # Alignment
-        sys.stdout.write(strftime("%Y-%m-%d %H:%M:%S") + ": Alignment with LAST\n")
-        call("lastdb ref_genome " + ref, shell=True)
-        call("lastal -a 1 ref_genome " + in_fasta + " | grep '^s ' > " + out_maf, shell=True)
 
-        # get best hit and unaligned reads
-        unaligned_length = list(get_besthit.besthit_and_unaligned(in_fasta, out_maf, outfile))
+    # If either of the alignment files are not provided (sam files):
+    if alignment_genome == '' or alignment_transcriptome == '':
+        alignment_genome = outfile + "_genome_alnm.sam"
+        alignment_transcriptome = outfile + "_transcriptome_alnm.sam"
 
-    # ALIGNED READS ANALYSIS
+        # Alignment to reference genome
+        sys.stdout.write(strftime("%Y-%m-%d %H:%M:%S") + ": Alignment with minimap2 to reference genome\n")
+        call("minimap2 -ax splice " + ref_g + " " + in_fasta + " > " + alignment_genome, shell=True)
+        # Alignment to reference transcriptome
+        sys.stdout.write(strftime("%Y-%m-%d %H:%M:%S") + ": Alignment with minimap2 to reference transcriptome\n")
+        call("minimap2 -ax splice " + ref_t + " " + in_fasta + " > " + alignment_transcriptome, shell=True)
+
+
+    # Read the genome alignments to memory:
+    sys.stdout.write(strftime("%Y-%m-%d %H:%M:%S") + ": Reading the genome alignments\n")
+    dict_genome_alignment = {}
+    SAM_or_BAM_Reader = HTSeq.SAM_Reader
+    read_seq_file = SAM_or_BAM_Reader(alignment_genome)
+    for read in read_seq_file:
+        qname = read.read.name
+        #if read.aligned:
+        if qname not in dict_genome_alignment:
+            dict_genome_alignment[qname] = read
+
+    # Read the transcriptome alignments to memory:
+    sys.stdout.write(strftime("%Y-%m-%d %H:%M:%S") + ": Reading the transcriptome alignments\n")
+    dict_trx_alignment = {}
+    SAM_or_BAM_Reader = HTSeq.SAM_Reader
+    read_alignment_trx = SAM_or_BAM_Reader(alignment_transcriptome)
+    for read in read_alignment_trx:
+        qname = read.read.name
+        if qname not in dict_trx_alignment:
+            dict_trx_alignment[qname] = read
+
+    # Aligned reads analysis
     sys.stdout.write(strftime("%Y-%m-%d %H:%M:%S") + ": Aligned reads analysis\n")
     num_aligned = align.head_align_tail(outfile, num_bins)
+
+    # Un-aligned reads analysis
+    sys.stdout.write(strftime("%Y-%m-%d %H:%M:%S") + ": Un-aligned reads analysis\n")
+
+    unaligned_length = []
+    num_unaligned = 0
+    for qname in dict_trx_alignment:
+        t_read = dict_trx_alignment[qname]
+        if not t_read.aligned:
+            num_unaligned += 1
+            unaligned_length.append(len(t_read.read.seq))
 
     # Length distribution of unaligned reads
     out1 = open(outfile + "_unaligned_length_ecdf", 'w')
 
-    num_unaligned = len(unaligned_length)
     if num_unaligned != 0:
         max_length = max(unaligned_length)
         hist_unaligned, edges_unaligned = numpy.histogram(unaligned_length, bins=numpy.arange(0, max_length + 50, 50),
@@ -146,6 +207,7 @@ def main(argv):
 
     out1.close()
     del unaligned_length
+
 
     # MATCH AND ERROR MODELS
     sys.stdout.write(strftime("%Y-%m-%d %H:%M:%S") + ": match and error models\n")
